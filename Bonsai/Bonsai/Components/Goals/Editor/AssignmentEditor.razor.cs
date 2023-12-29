cb66fbb5de66f2fa7d1f2a5bc0e6a831a582e1db
Thomas Herrmann <therrm17@student.aau.dk>
2023-12-29 13:12:48 +0100

    Show editor component dynamically

--- Bonsai/Bonsai/Components/Goals/Editor/AssignmentEditor.razor.cs
+++ Bonsai/Bonsai/Components/Goals/Editor/AssignmentEditor.razor.cs
@@ -1,11 +1,40 @@
 using Bonsai.Model;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace Bonsai.Components.Goals.Editor
 {
 	public partial class AssignmentEditor : IGoalEditorComponent<AssignmentGoal>
 	{
+		private EditContext? editContext;
+		private bool isInitialized;
+
 		[Parameter]
 		public AssignmentGoal? Goal { get; set; }
+
+		public bool TrySubmit(out AssignmentGoal? goal)
+		{
+			if (!isInitialized) throw new InvalidOperationException();
+
+			if (!editContext!.Validate())
+			{
+				goal = null;
+
+				return false;
+			}
+
+			goal = Goal;
+
+			return true;
+		}
+
+		protected override void OnInitialized()
+		{
+			Goal ??= new AssignmentGoal { CommencementTime = DateTime.Now, Deadline = DateTime.Today.AddDays(1), Title = "", Description = "", SubGoals = new List<Goal>() };
+			editContext = new EditContext(Goal);
+			isInitialized = true;
+
+			base.OnInitialized();
+		}
 	}
 }
--- Bonsai/Bonsai/Components/Goals/IGoalEditorComponent.cs
+++ Bonsai/Bonsai/Components/Goals/IGoalEditorComponent.cs
@@ -4,5 +4,12 @@ namespace Bonsai.Components.Goals
 {
 	public interface IGoalEditorComponent<TGoal> : IGoalComponent<TGoal> where TGoal : Goal
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="goal"></param>
+        /// <exception cref="InvalidOperationException"></exception>
+        /// <returns></returns>
+        public bool TrySubmit(out TGoal? goal);
     }
 }
